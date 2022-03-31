d3b41399d8d254c31a5f89e09470ffc6eb294fb3
darien24 <darien.kentanu@ottencoffee.co.id>
2022-04-01 02:39:50 +0700

    doctrine delete

---
+++ src/AppBundle/Controller/DeleteProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DeleteProductController extends Controller{
+
+    public function deleteproductAction($productId){
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $product = $repository->find($productId);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$productId
+            );
+        }
+        $entityManager->remove($product);
+        $entityManager->flush();
+        return new JsonResponse(array(
+            "status" => 200,
+            "message" => "product with id" . $productId . "successfully deleted"
+        ));
+    }
+}
